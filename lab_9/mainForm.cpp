a5ac166b319d3d800cd67e8d4c9fc4657518e06e
alecsmirnov <alecsmirnov93@gmail.com>
2019-10-14 09:47:22 +0700

    lab9 - comments added

--- lab_9/mainForm.cpp
+++ lab_9/mainForm.cpp
@@ -11,18 +11,23 @@
 
 TForm1* Form1;
 
+// Погрешность размера DBGrid относительно формы
 static constexpr auto DBGRID_CELL_FAULT  = 5;     
 static constexpr auto DBGRID_TABLE_FAULT = 45; 
 
+// Ширина и высота рамок вокруг DBGrid
 static constexpr auto BORDER_WIDTH  = 47;
 static constexpr auto BORDER_HEIGHT = 150;
 
+// Минимальные размеры окна формы
 static constexpr auto WINDOW_WIDTH_MIN  = 459;
 static constexpr auto WINDOW_HEIGHT_MIN = 124;
 
+// Позиции объектов пермещения
 static constexpr auto OBJ_POSITION_1 = 159;
 static constexpr auto OBJ_POSITION_2 = 262;
 
+// Верхняя граница процентов для первой задачи (task1)
 static constexpr auto HIGH_PERCENTAGE = 30;
 
 
@@ -33,9 +38,11 @@ __fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner) {
 }
 
 void __fastcall TForm1::FormShow(TObject* Sender) {
+	// Открываем соединение с бд перед демонстрацией формы
 	ADOConnection1->Open();
 }
 
+// Выполнение запроса в зависимости от выбора (чекбокса)
 void __fastcall TForm1::Button1Click(TObject* Sender) {
 	if (Form1->RadioButton1->Checked)
 		task1();
@@ -47,28 +54,34 @@ void __fastcall TForm1::Button1Click(TObject* Sender) {
 		task3();
 }
 
+// Очистка полей для ввода аргументов запроса
 void __fastcall TForm1::Button2Click(TObject* Sender) {
 	Form1->Edit1->Clear();
 	Form1->Edit2->Clear();
 }
 
+// Выобор первого запроса и смена отображения для получения данных
 void __fastcall TForm1::RadioButton1Click(TObject* Sender) {
 	viewSelect();
 }
 
+// Выбор второго запроса и смена отображения для ввода и получения данных
 void __fastcall TForm1::RadioButton2Click(TObject* Sender) {
 	viewInput("Изделие", "Год");
 }
 
-void __fastcall TForm1::RadioButton3Click(TObject *Sender) {
+// Выбор третьего запроса и смена отображения для ввода и получения данных
+void __fastcall TForm1::RadioButton3Click(TObject* Sender) {
 	viewInput("№ поставки", "Цена");
 }
 
+// Изменение размера формы
 void TForm1::resizeForm(int new_width, int new_height) {
 	Form1->Width = new_width;
 	Form1->Height = new_height;
 }
 
+// Вид отображение формы для получения данных
 void TForm1::viewSelect() {
 	Form1->Edit1->Hide();
 	Form1->Edit2->Hide();
@@ -81,35 +94,42 @@ void TForm1::viewSelect() {
 	Form1->Button1->Left = OBJ_POSITION_1;
 }
 
+// Вид отображение формы для ввода и получения данных
 void TForm1::viewInput(String edit1_hint, String edit2_hint) {
 	Form1->Edit1->Show();
 	Form1->Edit2->Show();
 	Form1->Button2->Show();
-
+	
+	// Названия вводимых данных
 	Form1->Edit1->TextHint = edit1_hint;
 	Form1->Edit2->TextHint = edit2_hint;
 
 	Form1->ADOQuery1->Close();	
 	DBGrid1->Hide();
 	resizeForm(WINDOW_WIDTH_MIN, WINDOW_HEIGHT_MIN);
-
+	
+	// Добавление на форму элементов для ввода
 	Form1->Edit1->Left = OBJ_POSITION_1;
 	Form1->Edit2->Left = OBJ_POSITION_1;
 	Form1->Button1->Left = OBJ_POSITION_2;
 }
 
+// Вывод окна сообщения об ощибках
 void TForm1::errorMessage(String text) {
 	Application->MessageBox(text.c_str(), L"Ошибка", MB_ICONERROR);
 }
 
+// Вывод окна сообщения с предупреждением
 void TForm1::warningMessage(String text) {
 	Application->MessageBox(text.c_str(), L"Внимание", MB_ICONWARNING);
 }
 
+// Вывод окна сообщения с результатом
 void TForm1::resultMessage(String text) {
 	Application->MessageBox(text.c_str(), L"Результат", NULL);
 }
 
+// Проверка строки на целое число
 bool TForm1::isIntValue(String string) {
 	AnsiString ansi_str(string.c_str());
 	std::string s(ansi_str.c_str());
@@ -121,6 +141,7 @@ bool TForm1::isIntValue(String string) {
 	return !s.empty() && it == s.end();
 }
 
+// Закраска строк DBGrid в зависимости от условия (Процент продаж не меньше значения HIGH_PERCENTAGE) 
 void __fastcall TForm1::DBGrid1DrawColumnCell(TObject* Sender, const TRect &Rect,
 											  int DataCol, TColumn* Column, TGridDrawState State) {
 	if (Form1->RadioButton1->Checked && HIGH_PERCENTAGE <= Form1->ADOQuery1->FieldByName("percent")->AsInteger) {	
@@ -131,26 +152,36 @@ void __fastcall TForm1::DBGrid1DrawColumnCell(TObject* Sender, const TRect &Rect
 	}
 }
 
+// Обработка таблицы, полученной в результате запроса
 int TForm1::processTable(const std::vector<String>& table_headers) {
+	// Получение размера шрифта таблицы
 	static const auto DBGRID_FONT_SIZE = DBGrid1->Canvas->Font->Size;			
 	
+	// Количество обработанных строк
 	int rows_processed = 0;
+	// Максимальная ширана столбцов
 	std::vector<int> columns_width(table_headers.size(), 0);
 	
+	// Перемещение указателя текущей записи
 	Form1->ADOQuery1->First();
 	while (!Form1->ADOQuery1->Eof) {
 		for (auto i = 0; i < table_headers.size(); ++i) {
+			// Ширина столбца текущей колонки
 			auto column_len = Form1->ADOQuery1->Fields->Fields[i]->AsString.Length();
 			
+			// Поиск максимальной ширины
 			if (columns_width[i] < column_len)
 				columns_width[i] = column_len;
 		}
-
+		
+		// Перевод указателя к следующей записи
 		Form1->ADOQuery1->Next();
 		
+		// Подсчёт количества обработанных записей
 		++rows_processed;
 	}
-
+	
+	// Формирование ширины каждого столбца
 	auto dbgrid_width = 0;
 	for (auto i = 0; i < table_headers.size(); ++i) {
 		DBGrid1->Columns->Items[i]->Title->Caption = table_headers[i];
@@ -163,9 +194,11 @@ int TForm1::processTable(const std::vector<String>& table_headers) {
 		DBGrid1->Columns->Items[i]->Width = cell_size;
 		dbgrid_width += cell_size;
 	}
-
+	
+	// Расчёт ширины таблицы по размерам колонок
 	DBGrid1->Width = dbgrid_width + DBGRID_TABLE_FAULT;
 
+	// Изменение размера формы под ширину таблицы
 	auto new_width = DBGrid1->Width + BORDER_WIDTH;
 	auto new_height = DBGrid1->Height + BORDER_HEIGHT;
 	if (DBGrid1->Width + BORDER_WIDTH < WINDOW_WIDTH_MIN )
@@ -176,40 +209,56 @@ int TForm1::processTable(const std::vector<String>& table_headers) {
 	return rows_processed;
 }
 
+// Выполнение SELECT запросов
 void TForm1::selectQuery(String query_text, const std::vector<String>& table_headers) {	
 	try {
+		// Начало транзакции
 		Form1->ADOConnection1->BeginTrans();
-
+		
+		// Прекращение работы запроса
 		Form1->ADOQuery1->Close();
 		
+		// Очистка и формирование нового SQL запроса
 		Form1->ADOQuery1->SQL->Clear();
 		Form1->ADOQuery1->SQL->Text = query_text;
-	
+		
+		// Установка связи между запросом и DataSet для отображения в DBGrid
 		Form1->DataSource1->DataSet = Form1->ADOQuery1;
+		// Открытие запроса
 		Form1->ADOQuery1->Open();
-	
+		
+		// Формирование таблицы и получения кол-ва обработанных строк
 		auto row_processed = processTable(table_headers);
-
+		
+		// Подтверждаем транзакцию (сделанные изменения)
 		Form1->ADOConnection1->CommitTrans();
-
-		if (0 < row_processed)
+		
+		// Вывести результат выполнения запроса 
+		if (0 < row_processed) {
 			DBGrid1->Show();
+			Label1->Caption = "Записей обработано: " + IntToStr(row_processed);
+		}
 		else {
 			DBGrid1->Hide();
 			resizeForm(WINDOW_WIDTH_MIN, WINDOW_HEIGHT_MIN);
 			resultMessage("Записей обработано: 0");
 		}
-
-		Label1->Caption = "Записей обработано: " + IntToStr(row_processed);
 	} 
 	catch (Exception &exception) {
+		// Откатываем транзакцию в случае неудачи
 		Form1->ADOConnection1->RollbackTrans();
+		// Разрываем соединение
 		Form1->ADOConnection1->Close();
 
 		errorMessage("Произошла ошибка при выполнении запроса!");
 	}
 }
 
+// 1) Для каждого изделия на конец каждого года получить:
+//    * размер максимальной поставки;
+//    * сумму, на которую выполнены поставки для изделия;
+//    * процент этой суммы от общей суммы по всем изделиям за год.
+// Упорядочить по году и проценту. Выделить строки, где процент не меньше 30.
 void TForm1::task1() {
 	auto query_text = "SELECT izd.year, izd.n_izd, izd.max_post, izd.total_sum_post, ROUND(izd.total_sum_post * 100 / total.sum::numeric, 2) AS percent\
 					   FROM (SELECT EXTRACT(year FROM pmib6706.spj.date_post) AS year, pmib6706.spj.n_izd, MAX(pmib6706.spj.kol * pmib6706.p.ves) AS max_post, SUM(pmib6706.spj.kol * pmib6706.spj.cost) AS total_sum_post\
@@ -223,16 +272,21 @@ void TForm1::task1() {
 							) total\
 					   ON izd.year = total.year\
 					   ORDER BY izd.year, percent";
-					   
+	
+	// Названия заголовков таблицы
 	std::vector<String> table_headers = {"Год", "Номер изделия", "Максимальная поставка", "Сумма поставок", "Процент"};
 	
 	selectQuery(query_text, table_headers);
 }
 
+// 2) Для указанных изделия и года по каждой поставке вывести:
+//    * сумму поставки;
+//    * разницу между ценой детали в поставке и средней ценой детали за год.
 void TForm1::task2() {
 	auto izd_name = Form1->Edit1->Text;
 	auto year = Form1->Edit2->Text;
 
+	// Проверка корректности вводимых данных
 	if (!izd_name.IsEmpty() && !year.IsEmpty())
 		if (isIntValue(year)) {
 			auto query_text = "SELECT post.n_spj, post.sum, ABS(post.cost - avg.avg_price) AS difference\
@@ -249,7 +303,8 @@ void TForm1::task2() {
 									 GROUP BY year, pmib6706.spj.n_izd\
 									) avg\
 							   ON post.year = avg.year AND post.n_izd = avg.n_izd";
-
+			
+			// Названия заголовков таблицы
 			std::vector<String> table_headers = {"Номер поставки", "Сумма", "Разность"};
 
 			selectQuery(query_text, table_headers);
@@ -260,10 +315,12 @@ void TForm1::task2() {
 		warningMessage("Значение поля для ввода не может быть пустым!");
 }
 
+// 3) Изменить цену детали в поставке.
 void TForm1::task3() {
 	auto n_post = Form1->Edit1->Text;
 	auto price = Form1->Edit2->Text;
 
+	// Проверка корректности вводимых данных
 	if (!n_post.IsEmpty() && !price.IsEmpty())
 		if (isIntValue(price)) {
 			auto query_text = "UPDATE pmib6706.spj\
@@ -271,22 +328,30 @@ void TForm1::task3() {
 							   WHERE pmib6706.spj.n_spj = \'" + n_post + "\'";
 			
 			try {
+				// Начало транзакции
 				Form1->ADOConnection1->BeginTrans();
 	
+				// Прекращение работы запроса
 				Form1->ADOQuery1->Close();
 				
+				// Очистка и формирование нового SQL запроса
 				Form1->ADOQuery1->SQL->Clear();
 				Form1->ADOQuery1->SQL->Text = query_text;
-	
+				
+				// Установка связи между запросом и DataSet для отображения в DBGrid
 				Form1->DataSource1->DataSet = Form1->ADOQuery1;
+				// Выполнение запроса на модификацию данных
 				Form1->ADOQuery1->ExecSQL();
-
+				
+				// Подтверждаем транзакцию (сделанные изменения)
 				Form1->ADOConnection1->CommitTrans();
-
+			
 				resultMessage("Записей обработано: " + IntToStr(Form1->ADOQuery1->RowsAffected));
             } 
 			catch (Exception &exception) {
+				// Откатываем транзакцию в случае неудачи
 				Form1->ADOConnection1->RollbackTrans();
+				// Разрываем соединение
 				Form1->ADOConnection1->Close();
 		
 				errorMessage("Произошла ошибка при выполнении запроса!");
--- lab_9/mainForm.h
+++ lab_9/mainForm.h
@@ -15,20 +15,64 @@
 
 
 class TForm1 : public TForm {
+public:
+	__fastcall TForm1(TComponent* Owner);
+	
 __published:
 	void __fastcall FormShow(TObject* Sender);
+	
+	// Выполнение запроса в зависимости от выбора
 	void __fastcall Button1Click(TObject* Sender);
-	void __fastcall Button2Click(TObject *Sender);
-	void __fastcall RadioButton1Click(TObject *Sender);
-	void __fastcall RadioButton2Click(TObject *Sender);
-	void __fastcall RadioButton3Click(TObject *Sender);
+	// Очистка полей для ввода аргументов запроса
+	void __fastcall Button2Click(TObject* Sender);
+	
+	// Выобор первого запроса и смена отображения для получения данных
+	void __fastcall RadioButton1Click(TObject* Sender);
+	// Выбор второго запроса и смена отображения для ввода и получения данных
+	void __fastcall RadioButton2Click(TObject* Sender);
+	// Выбор третьего запроса и смена отображения для ввода и получения данных
+	void __fastcall RadioButton3Click(TObject* Sender);
+	
+	// Закраска строк DBGrid в зависимости от условия
 	void __fastcall DBGrid1DrawColumnCell(TObject* Sender, const TRect &Rect, int DataCol,
 										  TColumn* Column, TGridDrawState State);
 
+private:
+	// Изменение размера формы
+	void resizeForm(int new_width, int new_height);
+	// Вид отображение формы для получения данных
+	void viewSelect();
+	// Вид отображение формы для ввода и получения данных
+	void viewInput(String edit1_hint, String edit2_hint);
+	
+	// Вывод окна сообщения об ощибках
+    void errorMessage(String text);
+	// Вывод окна сообщения с предупреждением
+	void warningMessage(String text);
+	// Вывод окна сообщения с результатом
+	void resultMessage(String text);
+	
+	// Проверка строки на целое число
+    bool isIntValue(String string);
+
+	// Обработка таблицы, полученной в результате запроса
+	int processTable(const std::vector<String>& table_headers);
+	// Выполнение SELECT запросов
+	void selectQuery(String query_text, const std::vector<String>& table_headers);
+
+	// Запросы
+	void task1();
+	void task2();
+	void task3();
+	
 __published:
+	// Связь приложения с Бд
 	TADOConnection* ADOConnection1;
+	// Формирование запроса к Бд
 	TADOQuery* ADOQuery1;
+	// Соединение DBGrid и подключенной Бд
     TDataSource* DataSource1;
+	// Табличное отображение данных
 	TDBGrid* DBGrid1;
 
 	TButton* Button1;
@@ -41,25 +85,6 @@ __published:
 	TEdit* Edit1;
 	TEdit* Edit2;
 	TLabel* Label1;
-
-public:
-	__fastcall TForm1(TComponent* Owner);
-
-private:
-	void resizeForm(int new_width, int new_height);
-	void viewSelect();
-	void viewInput(String edit1_hint, String edit2_hint);
-    void errorMessage(String text);
-	void warningMessage(String text);
-	void resultMessage(String text);
-    bool isIntValue(String string);
-
-	int processTable(const std::vector<String>& table_headers);
-	void selectQuery(String query_text, const std::vector<String>& table_headers);
-
-	void task1();
-	void task2();
-	void task3();
 };
 
 extern PACKAGE TForm1* Form1;
