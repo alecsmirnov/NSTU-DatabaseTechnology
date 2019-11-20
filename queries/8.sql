67661ebb7b73855b3e5badd56db4841498fd7d19
alecsmirnov <alecsmirnov93@gmail.com>
2019-11-20 21:43:41 +0700

    lab8 query fix

--- queries/8.sql
+++ queries/8.sql
@@ -12,32 +12,13 @@ LIMIT 1;
 
 UPDATE v 
 SET date_begin = date_begin - interval '1 month'
-WHERE v.n_v IN (SELECT max_v.n_v
-                FROM q
-                JOIN (SELECT v.n_v, v.n_izd, max_date.date
-                      FROM v
-                      CROSS JOIN (SELECT v.n_izd, MAX(v.date_begin) AS date
-                                  FROM v
-                                  GROUP BY v.n_izd
-                                 ) max_date
-                      WHERE v.n_izd = max_date.n_izd AND v.date_begin = max_date.date
-                     ) AS max_v
-                ON q.n_izd = max_v.n_izd
-                WHERE q.n_det = 'P2');
-
--- вариант 2
-
-UPDATE v 
-SET date_begin = date_begin - interval '1 month'
-WHERE v.n_v IN (SELECT max_v.n_v
-                FROM q
-                JOIN (SELECT out_v.n_v, out_v.n_izd, out_v.date_begin
-                      FROM v AS out_v
-                      WHERE out_v.date_begin = (SELECT v.date_begin
-                                                FROM v
-                                                WHERE v.n_izd = out_v.n_izd
-                                                ORDER BY v.date_begin DESC
-                                                LIMIT 1)
-                     ) AS max_v
-                ON q.n_izd = max_v.n_izd
-                WHERE q.n_det = 'P2');
+WHERE v.n_v IN (SELECT (SELECT v.n_v
+                        FROM v
+                        WHERE v.n_izd = out_v.n_izd
+                        ORDER BY v.date_begin DESC
+                        LIMIT 1)
+                FROM (SELECT DISTINCT q.n_izd
+                      FROM q
+                      WHERE q.n_det = 'P5'
+                     ) out_v
+               );
