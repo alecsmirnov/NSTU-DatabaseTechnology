4f425030074e7c58cc0672fe5fb6b5c5a28abc67
alecsmirnov <alecsmirnov93@gmail.com>
2019-11-27 12:28:12 +0700

    fixes

--- queries/8_9_defend.sql
+++ queries/8_9_defend.sql
@@ -22,16 +22,16 @@ ON dets.n_det = posts.n_det;
 -- Лабораторная 9 - Вариант 11
 -- Для каждого клиента вывести его заказы в хронологическом порядке и вычислить нарастающим итогом уплаченную сумму и долг
 
-SELECT info.*, info.total - info.order AS debt
+SELECT info.*, info.order - info.total AS debt
 FROM (SELECT out_r.*, out_r.cost * out_r.kol AS price, 
              COALESCE((SELECT SUM(r.cost * r.kol)
                        FROM r
-                       WHERE r.n_cl = out_r.n_cl AND r.date_order < out_r.date_order), 0
+                       WHERE r.n_cl = out_r.n_cl AND (r.date_order < out_r.date_order OR r.date_order = out_r.date_order AND CAST(SUBSTR(r.n_real, 2) AS INTEGER) <= CAST(SUBSTR(out_r.n_real, 2) AS INTEGER))), 0
                       ) AS order,
              COALESCE((SELECT SUM(r.cost * r.kol)
                        FROM r
-                       WHERE r.n_cl = out_r.n_cl AND (r.date_pay < out_r.date_order OR r.date_pay = out_r.date_order AND r.n_real != out_r.n_real)), 0
+                       WHERE r.n_cl = out_r.n_cl AND (r.date_pay < out_r.date_order OR r.date_pay = out_r.date_order AND CAST(SUBSTR(r.n_real, 2) AS INTEGER) <= CAST(SUBSTR(out_r.n_real, 2) AS INTEGER))), 0
                       ) AS total
       FROM r AS out_r
-      ORDER BY out_r.n_cl, out_r.date_order
+      ORDER BY out_r.n_cl, out_r.date_order, CAST(SUBSTR(out_r.n_real, 2) AS INTEGER)
      ) info;
